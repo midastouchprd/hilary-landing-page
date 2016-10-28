149b5339fc9222cabfedd8e520dc19ff06fb6a0b
Tosin <midastouchproductions@gmail.com>
2016-10-28 16:44:27 -0500

    finish for deploying

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-$(document).ready(function (){
-  console.log("booya");
-  $('.parallax').parallax();
-  $(".button-collapse").sideNav();
+$(document).ready(function() {
+    console.log("booya");
+    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+
+    $('#menu-items a').on('click', function(event) {
+        event.preventDefault();
+
+        var section = this.getAttribute('href');
+        $('html,body').animate({
+                scrollTop: $(section).offset().top
+            },
+            1000, 'swing');
+    });
 })
-  
